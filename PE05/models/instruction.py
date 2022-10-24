491b03a4649e8bba14968f16f2ebfcc89a0c4195
HuidiH <heidi.liljeberg@edu.turkuamk.fi>
2022-10-24 10:59:50 +0300

    PE05 EX33

---
+++ PE05/models/instruction.py
@@ -0,0 +1,46 @@
+from extensions import db
+
+
+class Instruction(db.Model):
+    __tablename__ = 'instruction'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    steps = db.Column(db.String(2000))
+    tools = db.Column(db.String(2000))
+    cost = db.Column(db.Integer)
+    duration = db.Column(db.Integer)
+    is_publish = db.Column(db.Boolean(), default=False)
+    created_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now(), onupdate=db.func.now())
+
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
+    def data(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'steps': self.steps,
+            'tools': self.tools,
+            'cost': self.cost,
+            'duration': self.duration,
+            'user_id': self.user_id
+        }
+
+    @classmethod
+    def get_all_published(cls):
+        return cls.query.filter_by(is_publish=True).all()
+
+    @classmethod
+    def get_by_id(cls, instruction_id):
+        return cls.query.filter_by(id=instruction_id).first()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
